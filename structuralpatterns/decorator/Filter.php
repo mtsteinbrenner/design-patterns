316b03b261513e18f487a74cc9efab94f1607e81
mtsteinbrenner <mirko.steinbrenner@deinhandy.de>
2018-11-25 21:10:41 +0100

    add decorator exampe (array of strings filter)

---
+++ structuralpatterns/decorator/Filter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace console\models\designpatterns\structuralpatterns\decorator;
+
+class Filter implements FilterInterface
+{
+	protected $result;
+
+	public function __construct($fullList)
+	{
+		$this->result = $fullList;
+	}
+
+	public function filter(): void
+	{
+		// filter without a filter doesn't do much
+	}
+
+	public function getResults(): array
+	{
+		return $this->result;
+	}
+
+	public function updateResults(array $result): void
+	{
+		$this->result = $result;
+	}
+}
---
+++ structuralpatterns/decorator/FilterByFirstLetterDecorator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace console\models\designpatterns\structuralpatterns\decorator;
+
+class FilterByFirstLetterDecorator extends AbstractFilterDecorator
+{
+	public function filter(): void
+	{
+		$results = $this->filter->getResults();
+
+		foreach ($results as $key => $result) {
+			if ($result[0] === $this->term) {
+				unset($results[$key]);
+			}
+		}
+
+		$this->updateResults($results);
+
+		$this->filter->filter();
+	}
+}
